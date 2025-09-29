f9ec356582d62304c32987fce380eb149ebe6f2f
Daria <dariash0422@gmail.com>
2025-09-29 21:02:21 +0300

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 function filterArray(numbers, value) {
     let newArray= [];
 
-    for(let i = 0; i <= numbers.length; i++) {
+    for(let i = 0; i < numbers.length; i++) {
         if(numbers[i] > value) {
             newArray.push(numbers[i]);
         }
